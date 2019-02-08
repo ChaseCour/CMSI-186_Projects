2c5a56704b4570f782e75b09d2b2cb305a8960f7
ChaseCour <ccour@lion.lmu.edu>
2019-02-07 17:56:50 -0800

    base

---
+++ BoardGame Edits/Clue.java
@@ -0,0 +1,135 @@
+public class Clue {
+
+    private location;
+
+    private weapon;
+
+    private perp;
+
+
+
+    public Clue(String randomLocation, int randomWeapon, randomPerson) {
+
+        System.out.println("-------Who killed Mr Boddy?------");
+
+
+
+        // Get the location
+
+        if (randomLocation == 1) {
+
+            this.location = "billiard room";
+
+            return "billiard room";
+
+        } else if (randomLocation = 2) {
+
+            this.location = "kitchen"
+
+            return;
+
+        } else if (randomLocation == 3) {
+
+            this.location = "hall";
+
+        } else if (randomLocation == 4) {
+
+            this.location = "study"
+
+        } else if (randomLocation == 5) {
+
+            this.location = lounge;
+
+        } else if (randomLocation == 6) {
+
+            this.location = "dining room";
+
+        } else if (randomLocation == 7) {
+
+            this.location = "ballroom"
+
+        } else if (randomLocation == 8) {
+
+            this.location = "conservatory";
+
+            break;
+
+        } else if (randomLocation == 9) {
+
+            this.location = "library";
+
+
+
+        // **** There are NO ERRORS in Get the weapon ****
+
+        // Get the weapon
+
+        if (randomWeapon == 1) {
+
+            this.weapon = "candlestick";
+
+        } else if (randomWeapon == 2) {
+
+            this.weapon = "dagger";
+
+        } else if (randomWeapon == 3) {
+
+            this.weapon = "lead pipe";
+
+        } else if (randomWeapon == 4) {
+
+            this.weapon = "revolver";
+
+        } else if (randomWeapon == 5) {
+
+            this.weapon = "rope";
+
+        } else if (randomWeapon == 6) {
+
+            this.weapon = "monkey wrench";
+
+        }
+
+
+
+        // **** There are NO ERRORS in Get the perp ****
+
+        // Get the perp
+
+        if (randomPerson == 1) {
+
+            this.perp = "Miss Scarlet";
+
+        } else if (randomPerson == 2) {
+
+            this.perp = "Professor Plum";
+
+        } else if (randomPerson == 3) {
+
+            this.perp = "Mr. Green";
+
+        } else if (randomPerson == 4) {
+
+            this.perp = "Colonel Mustard";
+
+        } else if (randomPerson == 5) {
+
+            this.perp = "Mrs. White";
+
+        } else if (randomPerson == 6) {
+
+            this.perp = "Mrs. Peacock";
+
+        }
+
+    }
+
+
+
+    private void getWhoDunnit() {
+
+        return "It was " + perp + " in the " + this.location + " with the " + weapon + "!";
+
+    }
+
+}
---
+++ BoardGame Edits/GameNight.java
@@ -0,0 +1,53 @@
+public class gamenight {
+
+
+
+    public static Main(String args[]) {
+
+        System.out.println("It's board game night! Let's play clue.");
+
+
+
+        int round1Location = (int)Math.ceil(Math.random() * 8);
+
+        int round1Weapon = (int)Math.ceil(Math.random() * 6);
+
+        int round1Perp = (int)Math.ceil(Math.random() * 6);
+
+
+
+        Clue round1 = new Clue(int round1Location, int round1Weapon, "6");
+
+        System.out.println(round1.getWhoDunnit());
+
+
+
+        System.out.println("That was fun. Let's play a new round of Clue.");
+
+
+
+        int round2Location = (int)Math.ceil(Math.random() * 8);
+
+        int round2Weapon = (int)Math.ceil(Math.random() * 6);
+
+        int round2Perp = (int)Math.ceil(Math.random() * 6);
+
+
+
+        Clue round2 = new Clue();
+
+        System.out.println(round2.getWhoDunnit(4, 2, 1));
+
+
+
+        System.out.println("Wait - was that the same configuration as the first time? Who did it in the first round?");
+
+        System.out.println(round1.getWhoDunnit("billiard room", "revolver", "Mrs Peacock"));
+
+
+
+        System.out.println("Thanks for clearing that up!");
+
+    }
+
+}
