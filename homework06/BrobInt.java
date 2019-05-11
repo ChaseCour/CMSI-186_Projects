2af34ce453432872afc08f926f4361a6d59ef5b3
ChaseCour <46768080+ChaseCour@users.noreply.github.com>
2019-05-11 00:08:30 -0700

    Update BrobInt.java
    done

--- homework06/BrobInt.java
+++ homework06/BrobInt.java
@@ -7,10 +7,21 @@ public class BrobInt{
   private String tempString;
   private String a;
   private String b;
+  private static String c;
+  private String d;
   private char charA;
   private int[] brob;
   private String toStringA;
   private boolean carry;
+  public static final BrobInt ONE = new BrobInt("1");
+  public static final BrobInt ZERO = new BrobInt("0");
+  public static final BrobInt TEN = new BrobInt("10");
+
+  public static BrobInt valueOf( long value ){
+    c = String.valueOf(value);
+    BrobInt temp = new BrobInt(c);
+    return temp;
+  }
 
   public BrobInt( String value ) {
     a = value;
@@ -22,25 +33,136 @@ public class BrobInt{
     }
     }
 
+    public boolean equals (BrobInt z){
+      if (this.brob.length != z.brob.length){
+        return false;
+      }
+      for (int i = 0; i < this.brob.length; i++){
+        if(this.brob[i] != z.brob[i])
+          return false;
+      }
+      return true;
+    }
+
+    public int compareTo( BrobInt z ){
+      int i = this.brob.length;
+      if(this.equals(z)){
+        return 0;
+      }
+      if (this.getRealLength() > z.getRealLength()){
+        return 1;
+      }
+      if (this.getRealLength() < z.getRealLength()){
+        return -1;
+      }
+      while (i > 0){
+        if (this.brob[this.brob.length - i] > z.brob[this.brob.length - i]){
+          return 1;
+        }
+        if (this.brob[this.brob.length - i] < z.brob[this.brob.length - i]){
+          return -1;
+        }
+        i--;
+      }
+      return 0;
+    }
+
+    public BrobInt remainder( BrobInt z ){
+      BrobInt tempBrob = this;
+      BrobInt tempBrober;
+      int passed = z.getLength();
+      int had = this.getLength();
+      int i = passed;
+      int j = had;
+      tempString = "0";
+      BrobInt total;
+      BrobInt times = new BrobInt(tempString);
+      BrobInt one = new BrobInt("1");
+      while (tempBrob.biggerThan(z)){
+//        System.out.println(tempBrob.toString());
+//        System.out.println("we live");
+        times = times.add(one);
+        tempBrober = tempBrob.subtract(z);
+//        System.out.println(tempBrober.toString());
+        tempBrober = tempBrober.zeroLessBrob();
+        tempBrob = tempBrober;
+//        System.out.println(times.toString());
+      }
+      times.subtract(one);
+      return tempBrob;
+    }
+
+    public BrobInt addZeros (int zeros){
+//      System.out.println(zeros);
+      String temp = this.toString();
+//      System.out.println(this.toString());
+      for (int i = 0; i < zeros; i++){
+        temp = temp + "0";
+      }
+      BrobInt passer = new BrobInt(temp);
+  //    System.out.println(passer.toString());
+      return passer;
+    }
+
+    public BrobInt zeroLessBrob(){
+  //    System.out.println("we live");
+      String temp = "";
+      int i = 0;
+      int intTemp;
+      while (this.brob[i] == 0 && i < this.brob.length - 1){
+        i++;
+//        System.out.println("we live");
+      }
+      while (i < this.brob.length){
+//        System.out.println(this.brob.length);
+        intTemp = this.brob[i];
+        temp = temp + intTemp;
+        i++;
+      }
+      if (temp.equals("")){
+        BrobInt pass = new BrobInt("0");
+        return pass;
+      }
+      BrobInt pass = new BrobInt(temp);
+      return pass;
+    }
+
+    public int getRealLength(){
+      int longer = this.getLength();
+      int check = this.brob[0];
+      int i = 0;
+//      System.out.println(longer);
+      while (check == 0 && i < this.brob.length - 1){
+        i++;
+        longer--;
+        check = this.brob[i];
+        System.out.println(check);
+        System.out.println(longer);
+      }
+      return longer;
+    }
 
     public String getString (){
       return a;
     }
 
     public int getLength (){
-      return a.length();
+      return this.brob.length;
     }
 
     public String toString (){
       return a;
     }
 
+
+
     public boolean biggerThan (BrobInt z){
+//      System.out.println("we live");
       int i = this.brob.length;
-      if (this.getLength() > z.getLength()){
+      if (this.getRealLength() > z.getRealLength()){
         return true;
       }
-      if (this.getLength() < z.getLength()){
+      if (this.getRealLength() < z.getRealLength()){
         return false;
       }
       while (i > 0){
@@ -56,6 +178,14 @@ public class BrobInt{
     }
 
     public BrobInt add(BrobInt z){
+      BrobInt first = this.zeroLessBrob();
+      BrobInt second = z.zeroLessBrob();
+      if (first.toString().equals("0")){
+        return second;
+      }
+      if (second.toString().equals("0")){
+        return first;
+      }
       BrobInt tempBrob;
       int passed = z.getLength();
       int had = this.getLength();
@@ -67,15 +197,12 @@ public class BrobInt{
         if (i < 0 && j < 0 && carry){
           tempString = 1 + "" + tempString;
           carry = false;
-          i--;
-          j--;
         }
         if (i == 0 && j == 0){
           if (carry){
             tempString = 1 + "" + tempString;
           }
-          i--;
-          j--;
+          carry = false;
         }
         if (i == 0 && j < 0){
           if (carry){
@@ -86,18 +213,22 @@ public class BrobInt{
               tempString = "0" + tempString;
             }
             else{
-            tempString = 1 + "" + tempString;
+            tempString = tempInt + "" + tempString;
           }
           }
-          i--;
-          j--;
         }
         if (i < 0 && j == 0){
           if (carry){
+            tempInt = this.brob[i - 1] + 1;
+            if (tempInt > 9){
+              tempInt = 0;
+              carry = true;
+              tempString = "0" + tempString;
+            }
+            else{
             tempString = 1 + "" + tempString;
           }
-          i--;
-          j--;
+          }
         }
         if (i > 0 && j == 0){
           if (carry){
@@ -108,7 +239,6 @@ public class BrobInt{
           }
           carry = false;
           tempInt += z.brob[i - 1];
-          i--;
           if (tempInt > 9){
             tempInt -= 10;
             carry = true;
@@ -124,7 +254,6 @@ public class BrobInt{
           }
           carry = false;
           tempInt += this.brob[j - 1];
-          i--;
           if (tempInt > 9){
             tempInt -= 10;
             carry = true;
@@ -146,18 +275,56 @@ public class BrobInt{
           carry = true;
         }
           tempString = tempInt + "" + tempString;
+      }
+
+      if (i < 0 && j > 0){
+  //      System.out.println("we live   j");
+//        System.out.println(j);
+        if (carry){
+          tempInt = this.brob[j - 1] + 1;
+          carry = false;
+          if (tempInt > 9){
+            tempInt = 0;
+            carry = true;
+            tempString = "0" + tempString;
+          }
+          else{
+          tempString = tempInt + "" + tempString;
+        }
+        }
+        else{
+          tempInt = this.brob[j - 1];
+          tempString = tempInt + "" + tempString;
+        }
+      }
+      if (j < 0 && i > 0){
+//        System.out.println("we live   i");
+  //      System.out.println(i);
+        if (carry){
+          tempInt = z.brob[i - 1] + 1;
+          carry = false;
+          if (tempInt > 9){
+            tempInt = 0;
+            carry = true;
+            tempString = "0" + tempString;
+          }
+          else{
+          tempString = tempInt + "" + tempString;
+        }
+        }
+        else{
+          tempInt = z.brob[i - 1];
+          tempString = tempInt + "" + tempString;
+        }
+      }
         i--;
         j--;
-      }
-      else {
-        i--;
-        j--;
-      }
     }
     if (carry){
       tempString = 1 + "" + tempString;
     }
       tempBrob = new BrobInt(tempString);
+      tempBrob = tempBrob.zeroLessBrob();
       return tempBrob;
     }
 
@@ -265,7 +432,64 @@ public class BrobInt{
             }}
         }
       tempBrob = new BrobInt(tempString);
+      tempBrob = tempBrob.zeroLessBrob();
       return tempBrob;
     }
+
+    public BrobInt divide(BrobInt z){
+      BrobInt tempBrob = this;
+      BrobInt tempBrober;
+      int passed = z.getLength();
+      int had = this.getLength();
+      int i = passed;
+      int j = had;
+      tempString = "0";
+      BrobInt total;
+      BrobInt times = new BrobInt(tempString);
+      BrobInt one = new BrobInt("1");
+      while (tempBrob.biggerThan(z)){
+//        System.out.println(tempBrob.toString());
+//        System.out.println("we live");
+        times = times.add(one);
+        tempBrober = tempBrob.subtract(z);
+//        System.out.println(tempBrober.toString());
+        tempBrober = tempBrober.zeroLessBrob();
+        tempBrob = tempBrober;
+//        System.out.println(times.toString());
+      }
+      times.subtract(one);
+      return times;
+    }
+
+    public BrobInt multiply(BrobInt z){
+      BrobInt tempBrob;
+      BrobInt tempBrober;
+      int quickSum;
+      String toPass;
+      int passed = z.getLength();
+      int had = this.getLength();
+      int zeros;
+      int i = passed;
+      int j = had;
+      tempString = "0";
+      BrobInt total;
+      BrobInt sum = new BrobInt(tempString);
+      for (int k = 0; k < had; k++){
+        for (int m = 0; m < passed; m++){
+//          System.out.println(k + "     " + m);
+          quickSum = this.brob[had - 1 - k] * z.brob[passed - 1 - m];
+          zeros = k + m;
+          toPass = quickSum + "";
+//          System.out.println(zeros);
+          tempBrober = new BrobInt(toPass);
+          tempBrob = tempBrober.addZeros(zeros);
+//          System.out.println(tempBrob.toString());
+          sum = sum.add(tempBrob);
+//          System.out.println(sum.toString());
+        }
+      }
+      return sum;
+    }
+
 
     }
